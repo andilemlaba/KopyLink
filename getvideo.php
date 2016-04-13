afecd21129075f880a59edb0d0c0e89a5d2eab0d
Andile Gumede <andile069@gmail.com>
2016-04-13 17:48:08 +0200

    Test

--- getvideo.php
+++ getvideo.php
@@ -64,7 +64,7 @@ if(isset($_REQUEST['videoid'])) {
 	echo '<p>No video id passed in</p>';
 	exit;
 }
-
+echo "this has been executed!!!";
 if(isset($_REQUEST['type'])) {
 	$my_type =  $_REQUEST['type'];
 } else {
@@ -98,7 +98,7 @@ if ($my_type == 'Download') {
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	</head>
-  
+
   <body>
       <?php
       } // end of if for type=Download
